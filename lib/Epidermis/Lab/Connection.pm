b98e9c4f95fdde0d978605a8aa89052693685fc5
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-03-28 23:33:56 -0500

    Add type for handle

--- lib/Epidermis/Lab/Connection.pm
+++ lib/Epidermis/Lab/Connection.pm
@@ -1,9 +1,13 @@
 package Epidermis::Lab::Connection;
 
 use Moo;
+use MooX::Should;
+
+use Types::Standard qw(InstanceOf);
 
 has handle => (
 	is => 'rwp',
+	should => InstanceOf['IO::Handle'],
 	init_arg => undef,
 );
 
