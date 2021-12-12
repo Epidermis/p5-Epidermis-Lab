175a2ecddbc3bc785a2f51742795ba0b62b14325
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-12-12 11:59:57 -0500

    Add helper for setting IO::Async handle setup

--- lib/Epidermis/Lab/Connection/Role/HandlesTwoDir.pm
+++ lib/Epidermis/Lab/Connection/Role/HandlesTwoDir.pm
@@ -16,6 +16,13 @@ has handle => (
 sub read_handle  { return $_[0]->handle; }
 sub write_handle { return $_[0]->handle; }
 
+sub io_async_setup_keep {
+	my ($self) = @_;
+	return (
+		$self->handle ,=> ['keep'],
+	);
+}
+
 with qw(Epidermis::Lab::Connection::Role::Handles);
 
 1;
